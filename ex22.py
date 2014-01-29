e59dce38f347316bf3c6844928c682146c78ae77
Daredel <gabo.fdc@gmail.com>
2014-01-29 16:08:56 -0500

    Excersices 1-34

---
+++ ex22.py
@@ -0,0 +1,11 @@
+print """
+%+
+d||i print number (decimal)
+o octal value
+x X hexadecimal
+e E float exponential form
+f float decimal form
+c char
+r string repr()
+s string str()
+"""
---
+++ ex34.py
@@ -0,0 +1,20 @@
+print """
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+    The animal at 1.
+    >python
+    The third (3rd) animal.
+    >peacock
+    The first (1st) animal.
+    >bear
+    The animal at 3.
+    >kangaroo
+    The fifth (5th) animal.
+    >whale
+    The animal at 2.
+    >python
+    The sixth (6th) animal.
+    >platypus
+    The animal at 4.
+    >kangaroo
+"""
